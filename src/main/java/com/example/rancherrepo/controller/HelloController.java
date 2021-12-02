ba391892e3d38b0c3e5e94a1cec897bebfa3969f
liuhz <997abc.xyz>
2021-12-02 14:12:28 +0800

    配置文件修改

---
+++ src/main/java/com/example/rancherrepo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.rancherrepo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/say")
+    public String say(String name){
+        return "hello: "+name;
+    }
+}
